d4a940f53df6d2ade915f5da8d49a67b160598a8
Spidious <lukedeffenbaugh@gmail.com>
2026-02-03 22:57:26 -0600

    Complete basic initial layout
    Completed the basics of the first layout. Subject to change
    
    Signed-off-by: Spidious <lukedeffenbaugh@gmail.com>

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,13 @@
-import { Text, View, StyleSheet} from "react-native";
+import { Text, View, StyleSheet, useWindowDimensions, Pressable } from "react-native";
 import {ImageBackground} from "expo-image";
 import { Stack } from 'expo-router';
 import {DefaultTheme} from "@react-navigation/native";
 
 export default function Index() {
+    const {height, width} = useWindowDimensions();
+
     return (
-        <View style={styles.container} >
+        <View style={{width:width, height:height}} >
             < Stack.Screen options={{ headerShown: false}} />
             <ImageBackground
                 style={[styles.image, styles.container]}
@@ -13,8 +15,20 @@ export default function Index() {
                     uri: "https://wallpapers.com/images/hd/pink-love-1920-x-1080-wallpaper-mzjkazybbea7amzt.jpg",
                 }}
             >
-                <View style={[styles.rect, styles.window]}>
-
+                <View style={[((width <= 500) ? styles.window_mobile : styles.window_desktop)]}>
+                    <View style={styles.container}>
+                        <Text style={{ fontSize:((width <= 500) ? 25 : 35), marginVertical: 20 }}>Will you be my Valentine?</Text>
+                        <View style={[styles.option_box, {flexDirection: (width <= 500) ? 'column' : 'row'}]}>
+                            <Pressable
+                                style={styles.button}
+                                onPress={()=> window.alert("Yes")}
+                            > Yes! </Pressable>
+                            <Pressable
+                                style={styles.button}
+                                onPress={()=> window.alert("No")}
+                            > No! </Pressable>
+                        </View>
+                    </View>
                 </View>
             </ImageBackground>
         </View>
@@ -31,15 +45,32 @@ const styles = StyleSheet.create({
         width: "100%",
         height: "100%",
     },
-    rect: {
-        borderRadius: 10,
-    },
-    window: {
-        width: '70%',
-        height: '70%',
-        minWidth: 500,
-        maxWidth: 800,
+    window_mobile: {
+        width: "100%",
+        height: "75%",
         backgroundColor: "white",
 
+    },
+    window_desktop: {
+        width: "60%",
+        height: "70%",
+        borderRadius: 15,
+        backgroundColor: "white",
+    },
+    option_box: {
+        height: "70%",
+        width: "90%",
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    button: {
+        margin: "auto",
+        backgroundColor: "pink",
+        borderRadius: 10,
+        width: "30%",
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
     }
 })
